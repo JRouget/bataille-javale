3475b96330ccf043b9c8b3e260491794b0b5709c
Florian <florian.caplet@Florians-MacBook-Air.local>
2026-03-12 13:49:36 +0100

    Feat : Structure moteur

--- src/main/java/school/coda/joshua_hugo/bataillejavale/moteur/GameManager.java
+++ src/main/java/school/coda/joshua_hugo/bataillejavale/moteur/GameManager.java
@@ -1,7 +1,19 @@
 package school.coda.joshua_hugo.bataillejavale.moteur;
 
+import school.coda.joshua_hugo.bataillejavale.map.Grille;
+import school.coda.joshua_hugo.bataillejavale.navires.Navire;
+import school.coda.joshua_hugo.bataillejavale.navires.TypeNavire;
+
 public class GameManager {
 
+    private Grille playerGrid;
+    private Grille computerGrid;
+    private Navire currentShip;
 
+    public GameManager() {
+        this.playerGrid = new Grille();
+        this.computerGrid = new Grille();
 
+        this.currentShip = new Navire(TypeNavire.PORTE_AVION);
+    }
 }
