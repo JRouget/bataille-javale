7765fa9b644980c1df46552d9b1150ea2b7c6169
JRouget <rougetjoshua@gmail.com>
2026-03-12 11:07:57 +0100

    map

---
+++ src/main/java/school/coda/joshua_hugo/bataillejavale/ui/accueil_controller.java
@@ -0,0 +1,11 @@
+package school.coda.joshua_hugo.bataillejavale.ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class accueil_controller {
+    @FXML
+    protected void lancerPartie(ActionEvent event) {
+        System.out.println("Bouton cliqué ! L'écran de placement des navires va charger...");
+    }
+}
